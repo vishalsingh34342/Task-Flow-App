b067f8d560a41ea49052f329b20e75bec9c74c78
Vishal Singh <jashanchoudhary430@gmail.com>
2026-04-03 10:04:02 +0530

    Added todolist component structure

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import Todoitem from './Todoitem'
 
 const Todolist = () => {
+  let todos = [];
+  let editingId;
+  let editText;
+  let onToggle;
+  let onStartEdit;
+  let onSaveEdit;
+  let onCancelEdit;
+  let onDelete;
+  let onEditTextChange;
+  let onEditKeyPress;
+
   return (
-    <div>Todolist</div>
+    <>
+      <div className='space-y-2'>
+        {todos.map((todo, index) => (
+          <Todoitem key={index} todo={todo} index={index}
+            editingId={editingId}
+            editText={editText}
+            onToggle={ontoggle}
+            onSaveEdit={onSaveEdit}
+            onCancelEdit={onCancelEdit}
+            onDelete={onDelete}
+            onEditTextChange={onEditTextChange}
+            onEditKeyPress={onEditKeyPress} />
+        ))}
+      </div>
+
+
+    </>
   )
 }
 
