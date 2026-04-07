699372d1cbe6d467c5db1673ca5b5ea52e0d331d
Vishal Singh <jashanchoudhary430@gmail.com>
2026-04-07 09:39:26 +0530

    Added empty state UI when no tasks are present

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Todoitem from './Todoitem'
+import { Sparkles } from 'lucide-react';
 
 const Todolist = () => {
   let todos = [];
@@ -12,6 +13,22 @@ const Todolist = () => {
   let onDelete;
   let onEditTextChange;
   let onEditKeyPress;
+
+  if (todos.length === 0) {
+    return (
+      <div className='text-center py-16 backdrop-blur-2xl bg-white/5 rounded-2xl border-white/10 '>
+        <div className='w-16
+         h-16 bg-linear-to-br from-violet-500/20  to-fuchsia-500/20 rounded-2xl flex items-center justify-center mx-auto mb-3'>
+          <Sparkles size={32} className='text-violet-300' />
+        </div>
+
+        <p className='text-white/90 text-base font-semibold mb-1'>No Task yet</p>
+        <p className='text-white/50 text-sm'>Create your first task to get started</p>
+
+
+      </div>
+    )
+  }
 
   return (
     <>
