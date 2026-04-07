ba7aaab3424ceacf875490588d3e3c8364930ee5
Vishal Singh <jashanchoudhary430@gmail.com>
2026-04-07 10:17:13 +0530

    Added notification component UI structure

--- src/components/Clearbutton.jsx
+++ src/components/Clearbutton.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const Clearbutton = () => {
+  let completedTodos;
+  let onClick;
+
+  if (completedTodos === 0) return null;
+
   return (
-    <div>Clearbutton</div>
+    <>
+      <button onClick={onclick} className='mt-4 w-full p-3 backdrop-blur-2xl bg-white/5 hover:bg-white/10 text-white/70 hover:text-white transition-all duration-300 rounded-lg font-semibold border-white/10 hover:scale-[1.02] active:scale-95 text-sm'>
+        clear{completedTodos} completed tasks
+      </button>
+
+    </>
+
   )
 }
 
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { CheckCircle, AlertCircle, X } from 'lucide-react';
 
 const Notification = () => {
+  // TODO: connect this with state later
+  let notification = null;
+  let onClose = () => { };
+
+  if (!notification) return null
   return (
-    <div>Notification</div>
+    <>
+      <div>
+
+        <div className='fixed top-4 right-4 z-50 animate-in slide-in-from-top-5'>
+          <div className={`flex items-center gap-3 px-3 py-3 rounded-2xl shadow-2xl backdrop-blur-2xl border ${notification.type === "success" ? "bg-emerald-500/90 border-emerald-400/50 text-white" : notification.type === "error" ? "bg-rose-500/50 border-rose-400/50 text-white" : "bg-blue-500/90 border-blue-400/50 text-white"
+            }`}>
+            {notification.type === "success" && <CheckCircle size={18} />}
+            {notification.type === "error" && <AlertCircle size={18} />}
+
+            <span className='font-semibold text-sm '>{notification.message}</span>
+            <button onClick={onClose} className='hover:opacity-80 transition-opacity'> <X size={16} /></button>
+
+
+          </div>
+
+
+        </div>
+      </div>
+    </>
   )
 }
 
